32fc635cdeecf42804c6cd22f0d3d8d0a65fc231
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-06-22 14:47:37 +0530

    setup routes using react-router-dom package

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import { Navbar, Home, PostDetail, CreatePost } from './'
+
 function App() {
   return (
-    <div className='App'>
-      <h1>React Blogs</h1>
+    <div className='container'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post/:id' element={<PostDetail />} />
+        <Route path='/create-post' element={<CreatePost />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import App from './App'
+import Home from './Home'
+import Navbar from './Navbar'
+import CreatePost from './CreatePost'
+import PostDetail from './PostDetail'
+
+export { App, CreatePost, PostDetail, Home, Navbar }
